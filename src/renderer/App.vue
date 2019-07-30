993ff01c776bface8f96f9596c9fb7854e507c6e
Erbolking <me@erickaky.com>
2019-07-30 09:52:09 +0200

    settings page WIP

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -15,38 +15,60 @@
         </router-link>
 
         <!-- Divider -->
-        <hr class="sidebar-divider my-0">
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+          Memories
+        </div>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item">
+        <li class="nav-item ">
           <router-link class="nav-link" to="/">
             <i class="fas fa-fw fa-plus"></i>
             <span>Create Memory</span>
           </router-link>
         </li>
 
-        <!--&lt;!&ndash; Divider &ndash;&gt;-->
-        <!--<hr class="sidebar-divider">-->
+<!--        <hr class="sidebar-divider my-0">-->
 
-        <!--&lt;!&ndash; Heading &ndash;&gt;-->
-        <!--<div class="sidebar-heading">-->
-          <!--Memories-->
-        <!--</div>-->
+        <li class="nav-item">
+          <router-link class="nav-link" to="/memories">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Memories</span>
+          </router-link>
+        </li>
+
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+          Settings
+        </div>
+
+        <li class="nav-item">
+          <router-link class="nav-link" to="/settings">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Settings</span>
+          </router-link>
+        </li>
 
         <!-- Nav Item - Pages Collapse Menu -->
-        <!--<li class="nav-item active">-->
-          <!--<a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">-->
-            <!--<i class="fas fa-fw fa-cog"></i>-->
-            <!--<span>Components</span>-->
-          <!--</a>-->
-          <!--<div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">-->
-            <!--<div class="bg-white py-2 collapse-inner rounded">-->
-              <!--<h6 class="collapse-header">Custom Components:</h6>-->
-              <!--<a class="collapse-item" href="buttons.html">Buttons</a>-->
-              <!--<a class="collapse-item active" href="cards.html">Cards</a>-->
-            <!--</div>-->
-          <!--</div>-->
-        <!--</li>-->
+<!--        <li class="nav-item active">-->
+<!--          <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">-->
+<!--            <i class="fas fa-fw fa-cog"></i>-->
+<!--            <span>Components</span>-->
+<!--          </a>-->
+<!--          <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">-->
+<!--            <div class="bg-white py-2 collapse-inner rounded">-->
+<!--              <h6 class="collapse-header">Custom Components:</h6>-->
+<!--              <a class="collapse-item" href="buttons.html">Buttons</a>-->
+<!--              <a class="collapse-item active" href="cards.html">Cards</a>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </li>-->
 
         <!--&lt;!&ndash; Nav Item - Utilities Collapse Menu &ndash;&gt;-->
         <!--<li class="nav-item">-->
